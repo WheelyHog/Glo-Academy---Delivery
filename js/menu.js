2bffb62c77621a6fe1c0677948be4db459dae5fb
WheelyHog <wheelyhog@gmail.com>
2021-11-06 13:47:17 +0100

    lesson 2

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const restaurant = 'food-band'
+
+const renderItems = (data) => {
+    console.log(data)
+
+    data.forEach(element => {
+        console.log(element)
+    });
+
+}
+
+fetch(`./db/${restaurant}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+        renderItems(data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
